12e724d3c13d39f91c852b07977b0f822f14147b
julian <julian.118@proton.me>
2024-01-16 00:19:41 -0500

    first commit

---
+++ vision.py
@@ -0,0 +1,44 @@
+from PIL import Image
+from dotenv import load_dotenv
+
+load_dotenv()  # Loading all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Function to load Gemini Pro model and get responses
+model = genai.GenerativeModel("gemini-pro-vision")
+def get_gemini_response(input, image):
+	if input != "":
+		response = model.generate_content([input, image])
+	else:
+		response = model.generate_content(image)
+	return response.text
+
+
+# Initialize our streamlit app
+
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Gemini Image Demo")
+input = st.text_input("Input Prompt: ", key="input")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
+image = ""
+if uploaded_file is not None:
+	image = Image.open(uploaded_file)
+	st.image(image, caption="Uploaded Image", use_column_width=True)
+
+
+submit = st.button("Tell me about the image")
+
+# if submit is clicked
+
+if submit is True:
+	response = get_gemini_response(input, image)
+	st.subheader("The Response is")
+	st.write(response)
